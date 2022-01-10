87247af33d5265ca37dd1a2974363f3adc7ed630
Mrinal Virnave <mrinalvirnave@users.noreply.github.com>
2022-01-09 16:09:18 -0800

    updated to include preflight

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const restify = require('restify'); //loads restify
 const mongoose = require('mongoose'); //loads mongoose
 const config = require('./config'); //loads our configuration file
 const rjwt = require('restify-jwt-community');  //loads 
+const corsMiddleware = require('restify-cors-middleware2'); // cors middleware
 const server = restify.createServer();
+const cors = corsMiddleware({
+    preflightMaxAge: 5, //Optional
+    origins: ['*'],
+    allowHeaders: ['Authorization']
+  })
 
 //Middleware
+server.pre(cors.preflight)
+server.use(cors.actual)
 server.use(restify.plugins.bodyParser())
 server.use(restify.plugins.queryParser());
 
